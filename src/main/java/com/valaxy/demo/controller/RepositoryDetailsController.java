1a48c5a86ffe27ac62a294bc1e747a1a126161c9
sandy171414 <133012293+sandy171414@users.noreply.github.com>
2025-07-08 17:49:36 +0530

    Update RepositoryDetailsController.java

--- src/main/java/com/valaxy/demo/controller/RepositoryDetailsController.java
+++ src/main/java/com/valaxy/demo/controller/RepositoryDetailsController.java
@@ -19,7 +19,7 @@ public class RepositoryDetailsController {
     public String getRepos() throws IOException {
         GitHub github = new GitHubBuilder().withPassword("valaxytech@gmail.com", "XXXXXXXX").build();
         GHRepositorySearchBuilder builder = github.searchRepositories();
-        return "wellcome DEV";
+        return "this is dev branch";
     }
 
     @GetMapping("/trends")
@@ -34,57 +34,7 @@ public class RepositoryDetailsController {
             trendDetails.put("MockTrend" + i, "https://twitter.com/MockTrend" + i);
         }
 
-<<<<<<< HEAD
-	@RequestMapping("/")
-	public String getRepos() throws IOException {
-		GitHub github = new GitHubBuilder().withPassword("valaxytech@gmail.com", "XXXXXXXX").build();
-		GHRepositorySearchBuilder builder = github.searchRepositories();
-		return "this is dev branch boom";
-	}
-
-	@GetMapping("/trends")
-	public Map<String, String> getTwitterTrends(@RequestParam("placeid") String trendPlace, @RequestParam("count") String trendCount) {
-		String consumerKey = env.getProperty("CONSUMER_KEY");
-		String consumerSecret = env.getProperty("CONSUMER_SECRET");
-		String accessToken = env.getProperty("ACCESS_TOKEN");
-		String accessTokenSecret = env.getProperty("ACCESS_TOKEN_SECRET");
-		System.out.println("consumerKey "+consumerKey+" consumerSecret "+consumerSecret+" accessToken "+accessToken+" accessTokenSecret "+accessTokenSecret);		
-		ConfigurationBuilder cb = new ConfigurationBuilder();
-		cb.setDebugEnabled(true)
-		        .setOAuthConsumerKey(consumerKey)
-				.setOAuthConsumerSecret(consumerSecret)
-				.setOAuthAccessToken(accessToken)
-				.setOAuthAccessTokenSecret(accessTokenSecret);
-		TwitterFactory tf = new TwitterFactory(cb.build());
-		System.out.println("Twitter Factory "+tf);
-		System.out.println("Code testing purpose ");
-		Twitter twitter = tf.getInstance();
-		System.out.println("Twitter object "+twitter);
-		Map<String, String> trendDetails = new HashMap<String, String>();
-		try {
-			Trends trends = twitter.getPlaceTrends(Integer.parseInt(trendPlace));
-			System.out.println("After API call");
-			int count = 0;
-			for (Trend trend : trends.getTrends()) {
-				if (count < Integer.parseInt(trendCount)) {
-					trendDetails.put(trend.getName(), trend.getURL());
-					count++;
-				}
-			}
-		} catch (TwitterException e) {
-			trendDetails.put("test", "MyTweet");
-            //trendDetails.put("Twitter Exception", e.getMessage());
-			System.out.println("Twitter exception "+e.getMessage());
-
-		}catch (Exception e) {
-			trendDetails.put("test", "MyTweet");
-            System.out.println("Exception "+e.getMessage());
-		}
-		return trendDetails;
-	}
-=======
         return trendDetails;
     }
->>>>>>> main
 
 }
